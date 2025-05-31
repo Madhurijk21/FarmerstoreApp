4e3d19ab6249cbb8728afed99a55da44da6c829f
Madhurijk21@gmail.com <madhurijk21@gmail.com>
2025-05-31 18:49:13 -0400

    Updated frontend, backend and admin panel changes

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -4,9 +4,19 @@ const orderSchema = new mongoose.Schema({
     userId:{type:String,required:true},
     items:{type:Array,required:true},
     amount:{type:Number,required:true},
-    address:{type:Object,required:true},
+     address: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipcode: { type: String, required: true },
+    country: { type: String, required: true },
+    phone: { type: String }
+  },
     status:{type:String,default:"Product Processing"},
-    data:{type:Date,default:Date.now()},
+    date: { type: Date, default: Date.now },
     payment:{type:Boolean,default:false}
 })
 
